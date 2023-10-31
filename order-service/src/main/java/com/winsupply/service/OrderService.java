0e66d9a2b778a00976a82e2c3a140e6701c3e309
pradeep7421 <147904815+pradeep7421@users.noreply.github.com>
2023-10-31 11:30:14 +0530

    Update OrderService.java
    order service committed

--- order-service/src/main/java/com/winsupply/service/OrderService.java
+++ order-service/src/main/java/com/winsupply/service/OrderService.java
@@ -6,7 +6,7 @@ import com.winsupply.model.OrderRequest;
 public interface OrderService {
 
     Order createOrder(OrderRequest orderRequest);
-
+System.out.println("print orderRequest");
     Order getOrderDetails(int orderId);
 
 
